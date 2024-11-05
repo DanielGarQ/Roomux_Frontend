52752082c3a086e83bf3c9f546734ac5e3ec5e16
Alejandro Gómez Orjuela <102133299+alejoDev117@users.noreply.github.com>
2024-11-05 10:23:18 -0500

    se conecta login y register

--- src/app/feature/login/login/login.component.spec.ts
+++ src/app/feature/login/login/login.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -8,10 +8,13 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      imports: [FormsModule], // Importa FormsModule para utilizar ngModel
+      declarations: [LoginComponent] // Debe estar en declarations, no en imports
     })
     .compileComponents();
-    
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/feature/login/register/register/register.component.ts
+++ src/app/feature/login/register/register/register.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-  tipoDocumento: string = '';
+  nombre: string = '';
+  correo: string = '';
+  id: string = '';
+  password: string = '';
+  confirmarPassword: string = '';
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  onRegister() {
+    if (this.password !== this.confirmarPassword) {
+      alert('Las contraseñas no coinciden. Por favor, verifica.');
+      return;
+    }
+
+    const url = 'http://localhost:8080/api/v1/register';
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const body = {
+      nombre: this.nombre,
+      id: this.id,
+      correo: this.correo,
+      password: this.password
+    };
+
+    this.http.post<{ message: string }>(url, body, { headers }).subscribe(
+      (response) => {
+        // Muestra un alert con el mensaje de éxito del servidor
+        alert('Registro exitoso: ' + response.message);
+        this.router.navigate(['/user-home']); // Redirige a la vista de login
+      },
+      (error) => {
+        // Muestra un alert con el mensaje de error del servidor
+        alert((error.error?.message || 'Error desconocido'));
+      }
+    );
+  }
+
+  onCancel() {
+    // Limpia los campos del formulario
+    this.nombre = '';
+    this.correo = '';
+    this.id = '';
+    this.password = '';
+    this.confirmarPassword = '';
+  }
 }
