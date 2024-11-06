4653c4092cb939d087c2028ffa821b4fac3e9cb5
DanielGarQ <danielgarciaquiceno@gmail.com>
2024-11-06 03:08:01 -0500

    user-home changes

--- src/app/feature/user-home/components/user-reservation-list/user-reservation-list.component.ts
+++ src/app/feature/user-home/components/user-reservation-list/user-reservation-list.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrl: './user-reservation-list.component.scss'
 })
 export class UserReservationListComponent {
+  showReservations = false; // Controla la visibilidad de la columna
+  reservas = [
+    { id: 1, nombre: 'Sala 101' },
+    { id: 2, nombre: 'Sala 202' },
+    { id: 3, nombre: 'Sala 303' }
+  ];
 
+  toggleReservations() {
+    this.showReservations = !this.showReservations; // Alterna visibilidad
+  }
 }
+
