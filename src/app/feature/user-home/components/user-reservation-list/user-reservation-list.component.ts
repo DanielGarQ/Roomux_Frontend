4a879affe595c72eb840a8fe7b03b0b84b56ecd3
DanielGarQ <danielgarciaquiceno@gmail.com>
2024-11-05 19:24:50 -0500

    CHANGES

---
+++ src/app/feature/user-home/components/user-reservation-list/user-reservation-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-reservation-list',
+  templateUrl: './user-reservation-list.component.html',
+  styleUrl: './user-reservation-list.component.scss'
+})
+export class UserReservationListComponent {
+
+}
--- src/app/feature/user-home/user-home.module.ts
+++ src/app/feature/user-home/user-home.module.ts
@@ -4,12 +4,14 @@ import { UserHomeComponent } from './components/user-home/user-home.component';
 import { UserHomeRoutingModule } from "./user-home-routing.module";
 import { SharedModule } from '../../shared/shared.module';
 import { ReservationListComponent } from './components/reservation-list/reservation-list.component';
+import { UserReservationListComponent } from './components/user-reservation-list/user-reservation-list.component';
 
 
 @NgModule({
   declarations: [
     UserHomeComponent,
     ReservationListComponent,
+    UserReservationListComponent
 
   ],
   imports: [
